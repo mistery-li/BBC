81181518a7ebf486dbd8ed51356244daa911b42e
mistery-li <853402477@qq.com>
2018-05-08 10:53:24 +0800

    category topic list

---
+++ database/seeds/TopicsTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Topic;
+use App\Models\User;
+use App\Models\Category;
+
+class TopicsTableSeeder extends Seeder
+{
+    public function run()
+    {
+        // 所有用户id 如【1,2,3】
+        $user_ids = User::all()->pluck('id')->toArray();
+
+        // 所有分类ID数组， 如【1,2,3,4】
+        $category_ids = Category::all()->pluck('id')->toArray();
+
+        $faker = app(Faker\Generator::class);
+
+        $topics = factory(Topic::class)
+            ->times(100)
+            ->make()
+            ->each(function ($topic, $index)
+            use ($user_ids, $category_ids, $faker) {
+                $topic->user_id = $faker->randomElement($user_ids);
+
+                $topic->category_id = $faker->randomElement($category_ids);
+            });
+
+        Topic::insert($topics->toArray());
+    }
+
+}
+
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = app(Faker\Generator::class);
+
+        $avatars = [
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/s5ehp11z6s.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/Lhd1SHqu86.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/LOnMrqbHJn.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/xAuDMxteQy.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/NDnzMutoxX.png?imageView2/1/w/200/h/200',
+        ];
+
+        $users = factory(User::class)
+            ->times(10)
+            ->make()
+            ->each(function ($user, $index)
+            use ($faker, $avatars) {
+                $user->avatar = $faker->randomElement($avatars);
+            });
+
+        // 隐藏字段可见，数据转为数组
+        $user_array = $users->makeVisible(['password', 'remember_token'])->toArray();
+
+        // 插入数据库
+        User::insert($user_array);
+
+        // 单独一个用户
+        $user = User::find(1);
+        $user->name = 'liii';
+        $user->email = '853402477@qq.com';
+        $user->avatar = 'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200';;
+        $user->save();
+    }
+}
