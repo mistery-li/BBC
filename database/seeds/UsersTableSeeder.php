21ea92f65369e11232fb194932cbe8b34534301b
mistery-li <litujin1123@gmail.com>
2018-05-09 18:33:35 +0800

    merge local

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = app(Faker\Generator::class);
+
+        $avatars = [
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/s5ehp11z6s.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/Lhd1SHqu86.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/LOnMrqbHJn.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/xAuDMxteQy.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/NDnzMutoxX.png?imageView2/1/w/200/h/200',
+        ];
+
+        // 生成数据集合
+        $users = factory(User::class)
+            ->times(10)
+            ->make()
+            ->each(function ($user, $index)
+            use ($faker, $avatars) {
+                $user->avatar = $faker->randomElement($avatars);
+            });
+
+        // 隐藏字段可见，数据集合转换为数组
+        $user_array = $users->makeVisible(['password', 'remember_token'])->toArray();
+
+        User::insert($user_array);
+
+        // 单独第一个用户处理
+        $user = User::find(1);
+        $user->name = 'liii';
+        $user->email = '853402477@qq.com';
+        $user->avatar = 'http://localhost/folder_name/1_1525697525_TiEZTS9hRM.jpg';
+        $user->save();
+    }
+}
